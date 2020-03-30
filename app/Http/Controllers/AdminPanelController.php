21024eec547b361f3f4367b323f129232ab789a0
Ivan Saltykov <hcys@list.ru>
2020-03-30 19:02:48 +0300

    Reports Job added

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\Jobs\StatisticsReport;
+use App\News;
 use App\Post;
+use App\Tag;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -29,5 +33,49 @@ class AdminPanelController extends Controller
         ]);
 
         return redirect('/admin');
+    }
+
+    public function reportsGeneration()
+    {
+        $reportsArray = \request('reports');
+        $reports = 'Отчёт.' . PHP_EOL;
+        if($reportsArray) {
+            foreach ($reportsArray as $key => $report) {
+                if(method_exists($this, $report)) {
+                    $reports .= $this->$report();
+                }
+            }
+        }
+
+        StatisticsReport::dispatch($reports, auth()->user());
+
+        flash('Отчёт запрошен');
+
+        return back();
+    }
+
+    protected function newsReport()
+    {
+        return 'Новостей: ' . News::count() . PHP_EOL;
+    }
+
+    protected function postsReport()
+    {
+        return 'Статей: ' . Post::count() . PHP_EOL;
+    }
+
+    protected function commentsReport()
+    {
+        return 'Комментариев: ' . Comment::count() . PHP_EOL;
+    }
+
+    protected function tagsReport()
+    {
+        return 'Тэгов: ' . Tag::count() . PHP_EOL;
+    }
+
+    protected function usersReport()
+    {
+        return 'Пользователей: ' . User::count() . PHP_EOL;
     }
 }
